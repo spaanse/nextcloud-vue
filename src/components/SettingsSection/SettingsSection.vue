f7676e462adaf3189f6bb8e6dc15b766b83936c7
Greta Doci <gretadoci@gmail.com>
2019-10-14 15:05:10 +0200

    add SettingsSection component
    Signed-off-by: Greta Doci <gretadoci@gmail.com>

---
+++ src/components/SettingsSection/SettingsSection.vue
@@ -0,0 +1,53 @@
+<!--
+  - @copyright Copyright (c) 2019 Julius Härtl <jus@bitgrid.net>
+  -
+  - @author Julius Härtl <jus@bitgrid.net>
+  - @author Greta Doci <gretadoci@gmail.com>
+  -
+  - @license GNU AGPL version 3 or any later version
+  -
+  - This program is free software: you can redistribute it and/or modify
+  - it under the terms of the GNU Affero General Public License as
+  - published by the Free Software Foundation, either version 3 of the
+  - License, or (at your option) any later version.
+  -
+  - This program is distributed in the hope that it will be useful,
+  - but WITHOUT ANY WARRANTY; without even the implied warranty of
+  - MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  - GNU Affero General Public License for more details.
+  -
+  - You should have received a copy of the GNU Affero General Public License
+  - along with this program. If not, see <http://www.gnu.org/licenses/>.
+  -
+  -->
+
+<template>
+	<div class="section">
+		<h2>{{ title }}</h2>
+		<p>{{ description }}</p>
+		<slot> </slot>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'SettingsSection',
+	components: {
+
+	},
+	props: {
+		initial: {
+			type: Object,
+			required: true
+		}
+	}
+}
+
+</script>
+
+<style scoped>
+	p {
+		margin-bottom: 15px;
+	}
+
+</style>

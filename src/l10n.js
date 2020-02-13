d8040c380ea0017a85a287c2da7108b542fef558
Christoph Wurst <christoph@winzerhof-wurst.at>
2020-02-13 14:08:15 +0100

    fixup! Add gettext-based translations
    Signed-off-by: Christoph Wurst <christoph@winzerhof-wurst.at>

--- src/l10n.js
+++ src/l10n.js
@@ -2,9 +2,7 @@ import GetText from 'node-gettext'
 import { getLocale } from '@nextcloud/l10n'
 
 const gt = new GetText()
-TRANSLATIONS.map(data => {
-	gt.addTranslations(data.locale, 'messages', data.json)
-})
+TRANSLATIONS.map(data => gt.addTranslations(data.locale, 'messages', data.json))
 gt.setLocale(getLocale())
 
 export default gt
